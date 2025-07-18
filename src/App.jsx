f7fae2937a73e6a8adf57158f0d3be6c99700028
Bubble457 <Nopphawat17@gmail.com>
2025-07-18 12:44:36 +0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='body'>
-      {/* <header className='head'>
-        <img src={logo} alt='Logo' width='300px' height='200px' />
-        <Link to='/setting' className='header2'>
-          <img src={setting} alt='Settings' width='auto' height='50px' />
-        </Link>
-      </header> */}
+    <div className='background'>
       <div className='buttongroup'>
         <Link to='/register' className='btnsignup'>ลงทะเบียน</Link>
         <Link to='/login' className='btnlogin'>เข้าสู่ระบบ</Link>
-        {/* <Link to='/intro'>1</Link> */}
       </div>
     </div>
   )
